3f9af570db542228d1c2ba1275d488c40fab402c
Sulani Paboda <sulanipaboda98@gmail.com>
2026-08-17 14:44:11 +0530

    update

--- app/Http/Controllers/V1/CustomerController.php
+++ app/Http/Controllers/V1/CustomerController.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 
